87f6e835726519167a7eadd2f2ded50cbf6ddb3b
Ralph Lee <dev.leeralph@gmail.com>
2024-05-04 11:34:03 +0800

    Add project files.

---
+++ PCCO104L.Classes.Demo/Avenger.cs
@@ -0,0 +1,93 @@
+﻿namespace PCCO104L.Classes.Demo
+{
+    // <access modifier> class <name>
+    public class Avenger
+    {
+        #region Fields
+        private string _heroName = string.Empty;
+        #endregion
+
+        #region Properties
+        public string HeroName
+        { 
+            get { return _heroName; }
+            set { _heroName = value; }
+        }
+
+        public string FirstName { get; set; } = "FirstName";
+        
+        private string _lastName = "LastName";
+        public string LastName
+        {
+            get { return _lastName.ToUpper(); }
+            set { _lastName = value.Replace("*", string.Empty); }
+        }
+
+        public string FullName 
+        {
+            get
+            {
+                return $"{HeroName} - {FirstName} {LastName}";
+            }
+        }
+
+        public int Damage { get; private set; }
+
+        private string _skill = string.Empty;
+        public string Skill
+        {
+            get 
+            {
+                if (string.IsNullOrEmpty(_skill)) { return "Normal Attack"; }
+                return _skill;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    _skill = string.Empty;
+                }
+                else
+                {
+                    _skill = value;
+                }
+            }
+        }
+        #endregion
+
+        #region Constructor and Constructor Overloading
+        public Avenger() { }
+
+        public Avenger(string heroName)
+        { 
+            HeroName=heroName;
+        }
+
+        public Avenger(string heroName, string first, string last) : this(heroName)
+        {
+            FirstName = first;
+            LastName = last;
+        }
+        #endregion
+
+        #region Methods and Method Overloading
+        public void SetDamage(int damage)
+        { 
+            Damage = damage;
+        }
+
+        public string Attack()
+        {
+            return $"{FullName} attacks using {Skill} with {Damage} damage.";
+        }
+
+        public string Attack(int damage, string skill)
+        {
+            SetDamage(damage);
+            _skill = skill;
+
+            return Attack();
+        }
+        #endregion
+    }
+}
---
+++ PCCO104L.Classes.Demo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.Metadata;
+
+namespace PCCO104L.Classes.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Without Class
+            string heroNameIronMan = "Iron Man";
+            string firstNameIronMan = "Tony";
+            string lastNameIronMan = "Stark";
+            string fullNameIronMan = $"{heroNameIronMan} - {firstNameIronMan} {lastNameIronMan}";
+
+            string heroNameCaptainAmerica = "CaptainAmerica";
+            string firstNameCaptainAmerica = "Steve";
+            string lastNameCaptainAmerica = "Rogers";
+            string fullNameCaptainAmerica = $"{heroNameCaptainAmerica} - {firstNameCaptainAmerica} {lastNameCaptainAmerica}";
+            #endregion
+
+            #region Using Class
+            Avenger ironMan = new Avenger();
+            ironMan.HeroName = "Iron Man";
+            ironMan.FirstName = "Tony";
+            ironMan.LastName = "Stark*";
+            ironMan.Skill = "Proton Cannon";
+            ironMan.SetDamage(150);
+            Console.WriteLine(ironMan.Attack());
+
+            Avenger captainAmerica = new Avenger
+            {
+                HeroName = "Captain America",
+                FirstName = "Steve",
+                LastName = "Rogers",
+                Skill = "Final Justice",
+            };
+            captainAmerica.SetDamage(120);
+            Console.WriteLine(captainAmerica.Attack());
+
+            Avenger blackWidow = new Avenger
+            {
+                HeroName = "Black Widow",
+                FirstName = "Natasha"
+            };
+            blackWidow.LastName = "Romanof";
+            Console.WriteLine(blackWidow.Attack());
+
+            Avenger hawkEye = new Avenger("HawkEye");
+            hawkEye.SetDamage(30);
+            Console.WriteLine(hawkEye.Attack());
+
+            Avenger hulk = new Avenger("Hulk", "Bruce", "Banner");
+            Console.WriteLine(hulk.Attack(100, "Hulk Smash"));
+            #endregion
+        }
+    }
+}
